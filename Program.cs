c6dd7723cf90adc01757b83574d7dc4ea547ea0f
Oleg <slobodchikov_1990@mail.ru>
2023-04-14 23:32:16 +0300

    solved task64

--- Program.cs
+++ Program.cs
@@ -5,3 +5,12 @@ N = 5 -> "5, 4, 3, 2, 1"
 N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 */
 
+int N;
+Console.WriteLine("Введите натуральное число:");
+int.TryParse(Console.ReadLine(), out N);
+Console.Write(OutNumbers(N));
+string OutNumbers (int N){
+    if (N<2){
+        return $"{1}";}
+    else {return $"{N}, {OutNumbers(N-1)}";}
+}
