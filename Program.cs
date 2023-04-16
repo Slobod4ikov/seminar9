bc4cb31587142cd05fc0f17c190b54b2d25484a6
Oleg <slobodchikov_1990@mail.ru>
2023-04-16 10:06:02 +0300

    solved task66

--- Program.cs
+++ Program.cs
@@ -4,3 +4,16 @@
 M = 1; N = 15 -> 120
 M = 4; N = 8. -> 30
 */
+int M, N;
+Console.WriteLine("Введите число M, M < N:");
+int.TryParse(Console.ReadLine(), out M);
+Console.WriteLine("Введите число N, N > M:");
+int.TryParse(Console.ReadLine(), out N);
+Console.Write(SumNumbers(M, N));
+int SumNumbers (int M, int N){
+    while (M<N+1)
+    {
+      return M+SumNumbers(M+1,N);  
+    }
+    return 0;
+}
